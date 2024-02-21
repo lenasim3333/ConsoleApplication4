deeddb0f7840ef8b507fd41c1e559655bd5bd3ff
enasim3333 <elenromans3@gmail.com>
2024-02-21 14:31:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int l, m, n;
+    cout << "Enter n and m - ";
+    cin >> n;
+    cin >> m;
+    l = m + n;
+    cout << " " << l;
+}
+
